5041ca2e517e443aa30113b1d9ba795d4080794a
aluno pos <alunopos@local.inatel.br>
2019-12-14 10:43:50 -0200

    Add models (Envelope,ProductEvent), modify model SnSMessage and add enum EventType

--- aws_project02/src/main/java/br/com/siecola/aws_project02/model/SnsMessage.java
+++ aws_project02/src/main/java/br/com/siecola/aws_project02/model/SnsMessage.java
@@ -1,7 +1,7 @@
 package br.com.siecola.aws_project02.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SnsMessage {
