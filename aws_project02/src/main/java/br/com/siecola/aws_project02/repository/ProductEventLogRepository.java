f8d5360ede51582f9766ecd187062aa26ea7e7f6
aluno pos <alunopos@local.inatel.br>
2019-12-14 15:16:09 -0200

    Create ProductEventLogController and add api methods

--- aws_project02/src/main/java/br/com/siecola/aws_project02/repository/ProductEventLogRepository.java
+++ aws_project02/src/main/java/br/com/siecola/aws_project02/repository/ProductEventLogRepository.java
@@ -4,6 +4,9 @@ import br.com.siecola.aws_project02.model.ProductEventLog;
 import org.springframework.data.repository.CrudRepository;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 
+import java.util.List;
+
 @EnableScan
 public interface ProductEventLogRepository extends CrudRepository<ProductEventLog, String> {
+    List<ProductEventLog> findAllByUsername(String username);
 }
