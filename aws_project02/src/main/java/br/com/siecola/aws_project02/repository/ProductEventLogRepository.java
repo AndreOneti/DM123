92a41e0cdb101dc2b18676cb24667aa9113e89d7
aluno pos <alunopos@local.inatel.br>
2019-12-14 14:21:19 -0200

    Create ProductEventLogRepository

---
+++ aws_project02/src/main/java/br/com/siecola/aws_project02/repository/ProductEventLogRepository.java
@@ -0,0 +1,9 @@
+package br.com.siecola.aws_project02.repository;
+
+import br.com.siecola.aws_project02.model.ProductEventLog;
+import org.springframework.data.repository.CrudRepository;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+
+@EnableScan
+public interface ProductEventLogRepository extends CrudRepository<ProductEventLog, String> {
+}
